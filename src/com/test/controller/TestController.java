e8941756f09ff96ebdccc36e0e6cfc913737f2c3
ankitjani <ankit.jani@techforceinfotech.in>
2016-12-29 15:50:20 +0530

    TestController added in the project.

---
+++ src/com/test/controller/TestController.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.test.controller;
+
+/**
+ * @author ankit
+ *
+ */
+public class TestController {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello World!");
+		
+	}
+
+}
