84e008e5b930be860e13323840d95684c73169c2
ankitjani <ankit.jani@techforceinfotech.in>
2017-01-02 14:35:50 +0530

    this is the latest one.

--- src/com/test/controller/TestController.java
+++ src/com/test/controller/TestController.java
@@ -15,11 +15,12 @@ public class TestController {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Hello World!");
 		int a = 10;
 		
 		String test = "This is test string";
 		System.out.println("a is ==> " + a + " and test is ==> " + test);
+		
+		System.out.println("This is the Latest.");
 	}
 
 }
